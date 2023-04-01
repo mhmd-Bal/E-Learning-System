4b153d0aa7ee122b631538da2e5768a8afca40f0
unknown <mohammad.balouza@gmail.com>
2023-04-02 01:10:48 +0300

    Added khaled's backend code after understanding it

---
+++ backend/routes/blog.routes.js
@@ -0,0 +1,15 @@
+const { Router } = require("express");
+const { getAllBlogs, getBlogById, createBlog, createComment, deleteComment } = require("../controllers/blog.controllers");
+const { authMiddleware } = require("../middlewares/auth.middleware");
+const { adminMiddleware } = require("../middlewares/admin.middleware");
+const router = Router();
+
+router.get("/", getAllBlogs);
+router.get("/:id", getBlogById);
+router.post("/", createBlog);
+
+router.post("/:id/comment", createComment)
+router.delete("/:blogId/comment/:commentId", adminMiddleware, deleteComment)
+
+
+module.exports = router;
