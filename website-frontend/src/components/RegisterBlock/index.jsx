89f65b05b8ca336fb4eba48226451005ed014438
unknown <mohammad.balouza@gmail.com>
2023-04-03 02:55:20 +0300

    Added frontend validation for the registration

--- website-frontend/src/components/RegisterBlock/index.jsx
+++ website-frontend/src/components/RegisterBlock/index.jsx
@@ -9,6 +9,8 @@ function RegisterBlock() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [username, setUsername] = useState('');
+    const [passworderror, setPasswordError] = useState('');
+    const [emailerror, setEmailerror] = useState('');
     
     
     const handleLoginNavigation = () => {
@@ -20,33 +22,51 @@ function RegisterBlock() {
     };
     
     const handleEmailChange = (e) => {
-        setEmail(e.target.value);
+        let value = e.target.value
+        setEmail(value);
+        setEmailerror('');
+        let validRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+        if(!value.match(validRegex)){
+            setEmailerror("This is not a valid Email Address");
+        }
     };
 
     const handlePasswordChange = (e) => {
-        setPassword(e.target.value);
+        let value = e.target.value
+        setPassword(value);
+        setPasswordError('');
+
+        if (value.length < 8 || !/[A-Z]/.test(value) || !/\d/.test(value) ||
+            !/[!@#$%^&*()_+={}\[\]:;<>?,./~\-]/.test(value)) {
+            setPasswordError(
+                'Password must be at least 8 characters long, contain at least one uppercase letter, one number, and one symbol'
+            );
+        }
     };
 
     const handleSubmit = async (e) => {
         e.preventDefault();
 
-        axios.post('http://127.0.0.1:8000/auth/register', {
-            username: username,
-            email: email,
-            password: password
-        }, {
-          headers: {
-            'content-type': 'application/json',
-            'Accept': 'application/json'
-          }
-        })
-          .then(response => {
-            console.log(response);
-            navigate('/login');
-          })
-          .catch(error => {
-            console.log(error);
-          });
+        if(emailerror == "" && passworderror == ""){
+            axios.post('http://127.0.0.1:8000/auth/register', {
+                username: username,
+                email: email,
+                password: password
+            }, {
+              headers: {
+                'content-type': 'application/json',
+                'Accept': 'application/json'
+              }
+            })
+              .then(response => {
+                console.log(response);
+                navigate('/login');
+              })
+              .catch(error => {
+                console.log(error);
+              });
+        }
     }
     
 
@@ -76,6 +96,8 @@ function RegisterBlock() {
                         name="email"
                         autoComplete="email"
                         onChange={handleEmailChange}
+                        error={emailerror ? true : false}
+                        helperText={emailerror ? emailerror : ''}
                     />
                     <TextField
                         margin="normal"
@@ -87,6 +109,8 @@ function RegisterBlock() {
                         id="password"
                         autoComplete="current-password"
                         onChange={handlePasswordChange}
+                        error={passworderror ? true : false}
+                        helperText={passworderror ? passworderror : ''}
                     />
                     <Stack direction="column" spacing={3} mt={3}>
                         <Button type="submit" fullWidth variant="contained">Sign Up</Button>
