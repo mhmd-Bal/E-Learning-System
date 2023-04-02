5d251af0aaa3e5bf93ea07546f51d623325c9326
unknown <mohammad.balouza@gmail.com>
2023-04-03 00:33:12 +0300

    Added handleSubmit function in the login page

--- website-frontend/src/components/LoginBlock/index.jsx
+++ website-frontend/src/components/LoginBlock/index.jsx
@@ -3,9 +3,33 @@ import { useNavigate } from "react-router-dom";
 
 function LoginBlock() {
     const navigate = useNavigate();
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [passwordError, setPasswordError] = useState('');
 
     const handleRegisterNavigation = () => {
         navigate("/register");
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        axios.post('http://127.0.0.1:3000/auth/login', {
+          email: email,
+          password: password
+        }, {
+          headers: {
+            'content-type': 'application/json',
+            'Accept': 'application/json',
+          }
+        })
+          .then(response => {
+            console.log(response);
+            localStorage.setItem('token', response.data.authorisation.token);
+            
+          })
+          .catch(error => {
+            alert("Incorrect Crendetials!");
+          });
     }
 
     return (
@@ -13,7 +37,7 @@ function LoginBlock() {
             <CssBaseline />
             <Box sx={{maxWidth: 415, mt: 8, display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
                 <Typography variant="h3">Sign in</Typography>
-                <Box component="form" noValidate mt={2}>
+                <Box component="form" onSubmit={handleSubmit} noValidate mt={2}>
                     <TextField
                         margin="normal"
                         required
