2b6ff6e3f09236b341dbed1d8f5358718a3f0f46
unknown <mohammad.balouza@gmail.com>
2023-04-02 21:25:23 +0300

    Added the CLassModel

---
+++ backend/models/classModel.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const classSchema = new mongoose.Schema({
+    class_name: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    class_description: {
+        type: String,
+        required: true,
+    }
+});
+
+const Class = mongoose.model("Class", classSchema);
+
+module.exports = Class;
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -2,6 +2,10 @@ const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 
 const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+  },
   email: {
     type: String,
     required: true,
