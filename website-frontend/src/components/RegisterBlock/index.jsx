81553e567697adda4fe0cbcd3819f497671af5c0
unknown <mohammad.balouza@gmail.com>
2023-04-02 14:06:46 +0300

    Added the register block

--- website-frontend/src/components/RegisterBlock/index.jsx
+++ website-frontend/src/components/RegisterBlock/index.jsx
@@ -1,13 +1,24 @@
 import {Box, Container, Typography, Checkbox ,CssBaseline, TextField, FormControlLabel, Button, Link, Stack} from '@mui/material';
 
-function LoginBlock() {
+function RegisterBlock() {
+    const wrongemail = true;
 
     return (
         <Container>
             <CssBaseline />
             <Box sx={{mt: 8, display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
-            <Typography variant="h3">Sign in</Typography>
+            <Typography variant="h3">Sign up</Typography>
             <Box component="form" noValidate sx={{ mt: 2 }}>
+                <TextField
+                    margin="normal"
+                    required
+                    fullWidth
+                    id="username"
+                    label="Username"
+                    name="username"
+                    autoComplete="username"
+                    autoFocus
+                />
                 <TextField
                     margin="normal"
                     required
@@ -16,7 +27,6 @@ function LoginBlock() {
                     label="Email Address"
                     name="email"
                     autoComplete="email"
-                    autoFocus
                 />
                 <TextField
                     margin="normal"
@@ -33,8 +43,8 @@ function LoginBlock() {
                         control={<Checkbox value="remember" color="primary" />}
                         label="Remember me"
                     />
-                    <Button type="submit" fullWidth variant="contained">Sign In</Button>
-                    <Link variant="body2">Don't have an account? Sign Up</Link>
+                    <Button type="submit" fullWidth variant="contained">Sign Up</Button>
+                    <Link variant="body2">Already have an account? Sign In</Link>
                 </Stack>
             </Box>
         </Box>
@@ -42,4 +52,4 @@ function LoginBlock() {
     )
 }
 
-export default LoginBlock;
+export default RegisterBlock;
---
+++ website-frontend/src/pages/RegisterPage/index.jsx
@@ -0,0 +1,16 @@
+import AppFooter from "../../components/Footer";
+import Navbar from "../../components/Navbar";
+import RegisterBlock from "../../components/RegisterBlock";
+
+
+function RegisterPage() {
+    return (
+        <>
+            <Navbar />
+            <RegisterBlock />
+            {/* <AppFooter /> */}
+        </>
+    );
+}
+
+export default RegisterPage;    
