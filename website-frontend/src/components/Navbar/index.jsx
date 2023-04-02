f1b22195da1eef95c5cb3c88d5fb514cfaaa774b
unknown <mohammad.balouza@gmail.com>
2023-04-02 13:07:15 +0300

    Handled the navigation

--- website-frontend/src/components/Navbar/index.jsx
+++ website-frontend/src/components/Navbar/index.jsx
@@ -1,14 +1,25 @@
 import "./styles.css";
 import { AppBar, Typography, Button, Box, Toolbar, Stack } from '@mui/material';
+import { useNavigate } from "react-router-dom";
 
 function Navbar() {
+    const navigate = useNavigate();
+
+    const handleLoginNavigation = () => {
+        navigate("/login");
+    }
+
+    const handleHomeNavigation = () => {
+        navigate("/");
+    }
+
     return (
         <>
             <AppBar position="relative">
                 <Toolbar>
-                    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>Logo</Typography>
+                    <Typography variant="h6" component="div" sx={{ flexGrow: 1, cursor: "pointer" }} onClick={handleHomeNavigation}>Logo</Typography>
                     <Stack direction="row" spacing={2}>
-                        <Button color="inherit" variant="outlined">Login</Button>
+                        <Button color="inherit" variant="outlined" onClick={handleLoginNavigation}>Login</Button>
                         <Button color="inherit" variant="outlined">Signup</Button>
                     </Stack>
                 </Toolbar>
