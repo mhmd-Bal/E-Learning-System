b461f86e7b2c322e04b6231c0ab2acf83e5dbee6
unknown <mohammad.balouza@gmail.com>
2023-04-03 03:01:11 +0300

    Added navbar changes depending on if the user is logged in

--- website-frontend/src/components/Navbar/index.jsx
+++ website-frontend/src/components/Navbar/index.jsx
@@ -3,6 +3,7 @@ import { useNavigate } from "react-router-dom";
 
 function Navbar() {
     const navigate = useNavigate();
+    const token = localStorage.getItem("token");
 
     const handleLoginNavigation = () => {
         navigate("/login");
@@ -16,6 +17,15 @@ function Navbar() {
         navigate("/register");
     }
 
+    const handleBrowseNavigation = () => {
+        navigate("/browse");
+    }
+
+    const handleLogout = () => {
+        localStorage.removeItem("token");
+        navigate("/");
+    }
+
     return (
         <>
             <AppBar position="relative">
@@ -23,8 +33,8 @@ function Navbar() {
                 <Toolbar>
                     <Typography variant="h6" component="div" sx={{ flexGrow: 1, cursor: "pointer" }} onClick={handleHomeNavigation}>Logo</Typography>
                     <Stack direction="row" spacing={2}>
-                        <Button color="inherit" variant="outlined" onClick={handleLoginNavigation}>Login</Button>
-                        <Button color="inherit" variant="outlined" onClick={handleRegisterNavigation}>Signup</Button>
+                        <Button color="inherit" variant="outlined" onClick={token ? handleBrowseNavigation : handleLoginNavigation}>{token ? "Browse" : "Login"}</Button>
+                        <Button color="inherit" variant="outlined" onClick={token ? handleLogout : handleRegisterNavigation}>{token ? "Logout" : "Sign Up"}</Button>
                     </Stack>
                 </Toolbar>
             </AppBar>
