c560ae2ddecb4459abfbb63f1fbbdfca334326ef
unknown <mohammad.balouza@gmail.com>
2023-04-03 04:00:41 +0300

    Added the display all classes functionality

--- website-frontend/src/components/AppClass/index.jsx
+++ website-frontend/src/components/AppClass/index.jsx
@@ -1,12 +1,12 @@
 import {Card, CardActions, CardContent, Typography, Button} from '@mui/material';
 
-function AppClass() {
+function AppClass(props) {
     return (
         <>
             <Card sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}>
                 <CardContent sx={{ flexGrow: 1 }}>
-                    <Typography gutterBottom variant="h4" paddingY={2}>Class Title</Typography>
-                    <Typography>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsam eos a veritatis consequatur, aliquam deleniti, eveniet natus reiciendis aspernatur dolor ea facere fugiat sit quod molestiae at officia perferendis soluta.</Typography>
+                    <Typography gutterBottom variant="h4" paddingY={2}>{props.class_name}</Typography>
+                    <Typography>{props.class_description}</Typography>
                 </CardContent>
                 <CardActions sx={{ paddingBottom: 3}}>
                     <Button variant='contained' size="small">Enroll</Button>
--- website-frontend/src/components/BrowseClasses/index.jsx
+++ website-frontend/src/components/BrowseClasses/index.jsx
@@ -1,19 +1,43 @@
 import AppClass from "../AppClass";
 import {Container, CssBaseline, Grid} from '@mui/material';
-
+import { useEffect } from "react";
+import { useState } from 'react';
+import axios from "axios";
 
 function BrowseClasses() {
+    const [courses, setCourses] = useState([]);
+
+    const token = localStorage.getItem("token");
+
+    useEffect( () => {
+        axios.get('http://127.0.0.1:8000/class', {
+            headers: {
+              'content-type': 'application/json',
+              'Accept': 'application/json',
+              'Authorization': 'bearer ' + token
+          }
+          })
+            .then( response => {
+                setCourses(response.data);
+            })
+            .catch(error => {
+              console.log(error);
+            });
+    }, [])
+    
     return (
-        <>
-            <Container sx={{ py: 8 }} maxWidth="lg">
-                <CssBaseline/>
-                <Grid container spacing={4}>
-                    <Grid item xs={12} sm={6} md={4}>
-                        <AppClass />
-                    </Grid>
-                </Grid>
-            </Container>
-        </>
+        <Container sx={{ py: 8 }} maxWidth="lg">
+            <CssBaseline/>
+            <Grid container spacing={4}>
+                {courses.map((element) =>{
+                    return(
+                        <Grid item xs={12} sm={6} md={4}>
+                            <AppClass class_description={element.class_description} class_name={element.class_name} class_id={element._id} />
+                        </Grid>
+                    )
+                })}
+            </Grid>
+        </Container>
     );
 }
 
