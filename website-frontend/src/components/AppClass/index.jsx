3d9cbfb0a303722714caee1c43fe99bd460128e2
unknown <mohammad.balouza@gmail.com>
2023-04-03 04:50:43 +0300

    Added the sending the Request form

--- website-frontend/src/components/AppClass/index.jsx
+++ website-frontend/src/components/AppClass/index.jsx
@@ -1,14 +1,29 @@
-import {Card, CardActions, CardContent, Typography, Button, Tooltip} from '@mui/material';
+import {Card, CardActions, CardContent, Typography, Button,TextField, Tooltip, Dialog, DialogTitle, DialogContentText, DialogContent, DialogActions} from '@mui/material';
 import { useState } from 'react';
 import axios from "axios";
 
 
 function AppClass(props) {
     const [enrolled, setEnrolled] = useState(false);
+    const [open, setOpen] = useState(false);
+    const [value, setValue] = useState('');
+
     const token = localStorage.getItem("token");
 
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+    
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+    const handleChange = (e) => {
+        setValue(e.target.value);
+    }
+    
     const checkIfEnrolled = () => {
-        
+
     }
 
     const handleEnrollInCourse = () => {
@@ -31,6 +46,28 @@ function AppClass(props) {
         }
     }
 
+    const handleRequestForm = () => {
+        if(enrolled){
+            axios.post('http://127.0.0.1:8000/form/request', {
+                class_id: props.class_id,
+                form_content: value
+            }, {
+              headers: {
+                'content-type': 'application/json',
+                'Accept': 'application/json',
+                'Authorization': 'bearer ' + token
+              }
+            })
+            .then(response => {
+                setOpen(false);
+                console.log(response);
+            })
+            .catch(error => {
+                console.log(error);
+            });
+        }
+    }
+
     return (
         <Card sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}>
             <CardContent sx={{ flexGrow: 1 }}>
@@ -38,12 +75,34 @@ function AppClass(props) {
                 <Typography>{props.class_description}</Typography>
             </CardContent>
             <CardActions sx={{ paddingBottom: 3}}>
-                <Button variant='contained' size="small" onClick={handleEnrollInCourse}>{ enrolled ? "Drop Out" : "Enroll"  }</Button>
+                <Button variant='contained' size="small" onClick={enrolled ? handleClickOpen : handleEnrollInCourse}>{ enrolled ? "Drop Out" : "Enroll"  }</Button>
                 <Tooltip title="There is no File at the moment" arrow>
                     <span style={{paddingLeft: 10}}>
                         <Button variant='outlined' disabled size="small">Download Document</Button>
                     </span>
                 </Tooltip>
+                <Dialog open={open} onClose={handleClose}>
+                    <DialogTitle>Withdrawal Form</DialogTitle>
+                    <DialogContent>
+                        <DialogContentText>
+                            What is your reason for withdrawing from the course ?
+                        </DialogContentText>
+                        <TextField
+                            autoFocus
+                            margin="dense"
+                            id="name"
+                            label="Reason"
+                            type="text"
+                            fullWidth
+                            variant="standard"
+                            onChange={handleChange}
+                        />
+                    </DialogContent>
+                    <DialogActions>
+                        <Button onClick={handleClose}>Cancel</Button>
+                        <Button onClick={handleRequestForm}>Send For Review</Button>
+                    </DialogActions>
+                </Dialog>
             </CardActions>
         </Card>
     );
