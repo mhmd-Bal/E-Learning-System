fa4f7a80c198c442e69399b7d16962e8a981c0f1
unknown <mohammad.balouza@gmail.com>
2023-04-03 04:21:48 +0300

    Added the enrolling into a course feature

--- website-frontend/src/components/AppClass/index.jsx
+++ website-frontend/src/components/AppClass/index.jsx
@@ -1,19 +1,51 @@
-import {Card, CardActions, CardContent, Typography, Button} from '@mui/material';
+import {Card, CardActions, CardContent, Typography, Button, Tooltip} from '@mui/material';
+import { useState } from 'react';
+import axios from "axios";
+
 
 function AppClass(props) {
+    const [enrolled, setEnrolled] = useState(false);
+    const token = localStorage.getItem("token");
+
+    const checkIfEnrolled = () => {
+        
+    }
+
+    const handleEnrollInCourse = () => {
+        if(!enrolled){
+            axios.post('http://127.0.0.1:8000/class/join', {
+                class_id: props.class_id
+            }, {
+              headers: {
+                'content-type': 'application/json',
+                'Accept': 'application/json',
+                'Authorization': 'bearer ' + token
+              }
+            })
+            .then(response => {
+                setEnrolled(true)
+            })
+            .catch(error => {
+                console.log(error);
+            });
+        }
+    }
+
     return (
-        <>
-            <Card sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}>
-                <CardContent sx={{ flexGrow: 1 }}>
-                    <Typography gutterBottom variant="h4" paddingY={2}>{props.class_name}</Typography>
-                    <Typography>{props.class_description}</Typography>
-                </CardContent>
-                <CardActions sx={{ paddingBottom: 3}}>
-                    <Button variant='contained' size="small">Enroll</Button>
-                    <Button variant='outlined' disabled size="small">Download Document</Button>
-                </CardActions>
-            </Card>
-        </>
+        <Card sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}>
+            <CardContent sx={{ flexGrow: 1 }}>
+                <Typography gutterBottom variant="h4" paddingY={2}>{props.class_name}</Typography>
+                <Typography>{props.class_description}</Typography>
+            </CardContent>
+            <CardActions sx={{ paddingBottom: 3}}>
+                <Button variant='contained' size="small" onClick={handleEnrollInCourse}>{ enrolled ? "Drop Out" : "Enroll"  }</Button>
+                <Tooltip title="There is no File at the moment" arrow>
+                    <span style={{paddingLeft: 10}}>
+                        <Button variant='outlined' disabled size="small">Download Document</Button>
+                    </span>
+                </Tooltip>
+            </CardActions>
+        </Card>
     );
 }
 
